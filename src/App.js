905610e40ed14f0e46a9fdd672e4e01124f60549
Neelesh Chandwani <neeleshchandwani@gmail.com>
2021-09-15 19:46:12 +0530

    React Router setup + usage

--- src/App.js
+++ src/App.js
@@ -3,24 +3,31 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Alert from './components/Alert';
 import TextForm from './components/TextForm';
-// import About from './components/About';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   const [mode, setmode] = useState('light');    //Default is light mode
 
   //To toggle b/w light and dark mode
   const toggleMode = () => {
-    if(mode==='light'){
+    if (mode === 'light') {
       setmode('dark');
       document.body.style.color = 'white';
       // document.body.style.backgroundColor = '#132231';
       document.body.style.backgroundColor = bg;
-      showAlert("Switched to dark mode","success");
-    }else{
+      showAlert("Switched to dark mode", "success");
+      document.title = "TextUtils - Dark mode";
+    } else {
       setmode('light');
       document.body.style.color = 'black';
       document.body.style.backgroundColor = 'white';
-      showAlert("Switched to light mode","success");
+      showAlert("Switched to light mode", "success");
+      document.title = "TextUtils - Light mode";
     }
   }
 
@@ -29,17 +36,17 @@ function App() {
 
   //Function which gets triggered when any of the color schemes is selected (default bluish)
   const colorSelector = (event) => {
-    if(event.target.value==='primary'){
+    if (event.target.value === 'primary') {
       setbg("#132231");
       setBtnColor("primary");
       document.body.style.backgroundColor = "#132231";
     }
-    else if(event.target.value==='success'){
+    else if (event.target.value === 'success') {
       setbg("#0a1c13");
       setBtnColor("success");
       document.body.style.backgroundColor = "#0a1c13";
     }
-    else{
+    else {
       setbg("#430b0b");
       setBtnColor("danger");
       document.body.style.backgroundColor = "#430b0b";
@@ -49,25 +56,36 @@ function App() {
   const [alert, setalert] = useState(null);
   //Function to show alerts 
   const showAlert = (message, type) => {
-                  setalert({
-                    message: message,
-                    type: type
-                  })
-                  //To make the alert disappear after 1.5 sec
-                  setTimeout(() => {
-                    setalert(null);
-                  }, 1500);
+    setalert({
+      message: message,
+      type: type
+    })
+    //To make the alert disappear after 1.5 sec
+    setTimeout(() => {
+      setalert(null);
+    }, 1500);
   }
 
 
   return (
     <>
-    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} colorSelector={colorSelector} />
-    <Alert alert={alert}/>
-    <div className="container">
-      <TextForm heading="Enter text for analysis here" mode={mode} showAlert={showAlert} bg={bg} btnColor={btnColor}/>
-      {/* <About mode={mode} bg={bg} /> */}
-    </div>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} colorSelector={colorSelector} />
+        <Alert alert={alert} />
+        <div className="container">
+          <Switch>
+            {/* The reason to use 'exact' keyword before path is, react matches the path partially & that can cause issues
+                /users  --> Component 1
+                /users/profile  --> Component 1 */}
+            <Route exact path="/about">
+              <About mode={mode} bg={bg} />
+            </Route>
+            <Route exact path="/">
+              <TextForm heading="Enter text for analysis here" mode={mode} showAlert={showAlert} bg={bg} btnColor={btnColor} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
